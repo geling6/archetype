e932051e96888ad7510542cb8509ee541e3674c2
ningxiner66@126.com <ningxiner66@126.com>
2017-06-07 19:41:14 +0800

    打包是jar，而不是war了.

--- src/main/java/com/geling/archetype/Application.java
+++ src/main/java/com/geling/archetype/Application.java
@@ -1,14 +1,10 @@
 package com.geling.archetype;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
