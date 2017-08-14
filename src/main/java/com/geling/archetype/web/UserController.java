c191c887993ccefd5d0ebfdd5c2868bff7dab99f
geling <ningxiner66@126.com>
2017-08-14 08:46:15 +0800

    bakup

--- src/main/java/com/geling/archetype/web/UserController.java
+++ src/main/java/com/geling/archetype/web/UserController.java
@@ -1,6 +1,7 @@
 package com.geling.archetype.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,4 +12,12 @@ public class UserController {
 	public void user(){
 		System.out.println("Hello user .");
 	}
+	
+	@RequestMapping("/{id}")
+	public String queryUser(@PathVariable Integer id){
+		System.out.println(id);
+		
+		return "fuck na wei !";
+	}
+	
 }
